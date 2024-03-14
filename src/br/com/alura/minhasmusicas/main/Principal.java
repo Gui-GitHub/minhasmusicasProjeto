1d25049894bcbd147b930535f43d90a50d9646c5
Gui-GitHub <guilhermecavalcante222@gmail.com>
2024-03-14 15:10:54 -0300

    Att

--- src/br/com/alura/minhasmusicas/main/Principal.java
+++ src/br/com/alura/minhasmusicas/main/Principal.java
@@ -1,2 +1,37 @@
-package br.com.alura.minhasmusicas.main;public class Principal {
+package br.com.alura.minhasmusicas.main;
+
+import br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica musica1 = new Musica();
+        musica1.setTitulo("Forever");
+        musica1.setArtista("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            musica1.reproduzir();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            musica1.curtir();
+        }
+
+        Podcast podcast1 = new Podcast();
+        podcast1.setTitulo("BolhaDev");
+        podcast1.setHost("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            podcast1.reproduzir();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            podcast1.curtir();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(podcast1);
+        preferidas.inclui(musica1);
+    }
 }
--- src/br/com/alura/minhasmusicas/modelos/Audio.java
+++ src/br/com/alura/minhasmusicas/modelos/Audio.java
@@ -1,2 +1,44 @@
-package br.com.alura.minhasmusicas.modelos;public class Audio {
+package br.com.alura.minhasmusicas.modelos;
+
+public class Audio {
+    private String titulo;
+    private int totalMin;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private int classificacao;
+
+    public void curtir(){
+        this.totalCurtidas++;
+    }
+    public void reproduzir(){
+        this.totalReproducoes++;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public int getClassificacao() {
+        return classificacao;
+    }
+
+    public int getTotalMin() {
+        return totalMin;
+    }
+
+    public void setTotalMin(int totalMin) {
+        this.totalMin = totalMin;
+    }
 }
